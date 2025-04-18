f1673b5e439deb2e6f23ae7b40ee15c8167dfbbf
pranjal.arora <pranjalarora98@gmail.com>
2025-04-18 18:00:02 +0530

    init commit

--- processor/src/index.ts
+++ processor/src/index.ts
@@ -1,13 +1,13 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-
-async function main(){
-while(1){
-  const list =  await prisma.zapRunOutbox.findMany({})
-  console.log(list);
+async function main() {
+  while (1) {
+    //comments added
+    const list = await prisma.zapRunOutbox.findMany({});
+    console.log(list);
+  }
 }
-}
 
-main()
+main();
